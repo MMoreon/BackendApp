534a90726b9abf9344d3a30f40350bf2e1b4b305
Никитос <mm0r30n@gmail.com>
2026-02-22 13:44:49 +0500

    подключение к БД

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import settings
 
+
 engine = create_engine(settings.DATABASE_URL,connect_args={"check_same_thread": False}) # флаг для потоков SQLite
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
